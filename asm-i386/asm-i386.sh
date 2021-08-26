4b84df9de67411dd65bf4c483856a8df846268b8
Hiroyuki Ohsaki <code@lsnl.jp>
2021-08-27 02:36:09 +0900

    add Docker

---
+++ asm-i386/asm-i386.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+name=`basename $0 .sh`
+tag=hohsaki/asm-i386:1.0
+
+run_opts='-d --restart always'
+while getopts 'f' var
+do
+    case "$var" in
+	f) run_opts='-it --rm' ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+case "$1" in
+    start)
+	docker run $run_opts \
+	       --name $name \
+	       --hostname $name \
+	       -p 3331:22 \
+	       -e TZ=Asia/Tokyo \
+	       $tag
+	;;
+    stop)
+	docker rm -f $name
+	;;
+    restart)
+	$0 stop
+	$0 start
+	;;
+    shell)
+	docker exec -it $name /bin/bash
+	;;
+esac
